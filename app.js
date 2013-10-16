088723cd1e3702ac359f8db7f216852dc054fa70
iharosi <iharosi@gmail.com>
2013-10-16 14:43:05 +0200

    frontend assets added

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var livef1  = require('./lib/livef1')
   ;
 
 var app = connect()
-  .use(connect.static('public'))
-  .listen(3000)
+  . use(connect.static('public', { maxAge: 31557600000 }))
+  . use(connect.compress())
+  . use(connect.favicon(__dirname + '/public/favicon.ico'))
+  . listen(3000)
   ;
 
 var io = require('socket.io').listen(app);
-io.set('log level', 2);
+    io.set('log level', 2);
+    io.enable('browser client gzip');
+    io.enable('browser client minification');
 
-var F1_USER = "a6042332@drdrb.com",
-    F1_PASS = "zb2MDAqYBwYPNbg9",
-    F1_KEYFRAME = __dirname + "/fixtures/2013-japan-suzuka/keyframe_00005.bin";
+var F1_USER = "a6042332@drdrb.com"
+  , F1_PASS = "zb2MDAqYBwYPNbg9"
+  , F1_KEYFRAME = false ? __dirname + "/fixtures/2013-japan-suzuka/keyframe_00005.bin" : "live"
+  ;
 
 io.sockets.on('connection', function (socket) {
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
-// document.addEventListener('touchmove', function(e) { e.preventDefault(); });
 var commentBox = document.querySelector("#commentBox"),
     eventTitle = document.querySelector("#eventTitle"),
-    splash     = document.querySelector("#splash");
+    splash     = document.querySelector("#splash"),
+    livetiming = document.querySelector("#livetiming>tbody");
 var socket = io.connect('http://192.168.110.220:3000');
 var commentary = {
     texts: [],
     bit: false,
     first: false
 }
+var carsArr = {};
+var liveTimingTable = "";
 
 socket.on('packet', function (data) {
 
@@ -34,8 +36,63 @@ socket.on('packet', function (data) {
         }
     }
 
-    // live timing
-    if (data.carId) {
+    // live timing datas
+    if (data.carId && data.carId >= 0) {
+        if (!(data.carId in carsArr)) {
+            carsArr[data.carId] = [];
+        }
+        switch (Object.keys(data)[0]) {
+            case 'history':
+                carsArr[data.carId][0] = data.history[0];
+                break;
+            case 'positionUpdate':
+                carsArr[data.carId][0] = data.positionUpdate;
+                break;
+            case 'position':
+                carsArr[data.carId][0] = data.position;
+                break;
+            case 'number':
+                carsArr[data.carId][1] = data.number;
+                break;
+            case 'driver':
+                carsArr[data.carId][2] = data.driver;
+                break;
+            case 'gap':
+                carsArr[data.carId][3] = data.gap;
+                break;
+            case 'interval':
+                carsArr[data.carId][4] = data.interval;
+                break;
+            case 'lapTime':
+                carsArr[data.carId][5] = data.lapTime;
+                break;
+            case 'sector1':
+                carsArr[data.carId][6] = data.sector1;
+                break;
+            case 'pitlap1':
+                carsArr[data.carId][7] = data.pitlap1;
+                break;
+            case 'sector2':
+                carsArr[data.carId][8] = data.sector2;
+                break;
+            case 'pitlap2':
+                carsArr[data.carId][9] = data.pitlap2;
+                break;
+            case 'sector3':
+                carsArr[data.carId][10] = data.sector3;
+                break;
+            case 'pitlap3':
+                carsArr[data.carId][11] = data.pitlap3;
+                break;
+            case 'numPits':
+                carsArr[data.carId][12] = data.numPits;
+                break;
+        }
+        for (var i in carsArr) {
+            carsArr[i].forEach(function(value, index){
+                // console.log(value);
+            });
+        }
         
     }
 
